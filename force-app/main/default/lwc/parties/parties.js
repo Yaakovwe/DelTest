24cb90a5d96bb145cea8bc28d78ec298630dd078
Yaakov Weizman <yaakov.weizman@novideasoft.com>
2023-03-21 22:48:15 +0200

    fix parties

--- force-app/main/default/lwc/parties/parties.js
+++ force-app/main/default/lwc/parties/parties.js
@@ -3,65 +3,67 @@ import getParties from "@salesforce/apex/PartyService.fetchParties";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class Parties extends LightningElement {
-	@track parties = [];
-	@track party = {};
-	currPageParties = [];
-	renderPagination = false;
-	hasPageChanged = false;
-	modalHeading = "New Party";
-	modalContentStyle = "height:65%";
-	modalGridStyleClass = "slds-medium-size_1-of-2";
-	showAddParty = true;
-	show = false;
+  @track parties = [];
+  @track party = {};
+  currPageParties = [];
+  renderPagination = false;
+  hasPageChanged = false;
+  modalHeading = "New Party";
+  modalContentStyle = "height:65%";
+  modalGridStyleClass = "slds-medium-size_1-of-2";
+  showAddParty = true;
+  show = false;
 
-	get isReady() {
-		return this.show;
-	}
-	get partiesToRender() {
-		return this.currPageParties;
-	}
-	async connectedCallback() {
-		const results = await getParties();
-		if (results) {
-            console.log(results);
-			this.parties = results;
-			this.renderPagination = true;
-			this.show = true;
-		} else {
-			console.log("oops");
-		}
-	}
+  get isReady() {
+    return this.showAddParty;
+  }
+  get partiesToRender() {
+    return this.currPageParties;
+  }
+  async connectedCallback() {
+    this.getParties();
+  }
 
-	handlePagination(event) {
-		this.hasPageChanged = !this.hasPageChanged;
-		this.currPageParties = event.detail.records;
-	}
+  handlePagination(event) {
+    this.hasPageChanged = !this.hasPageChanged;
+    this.currPageParties = event.detail.records;
+  }
 
-	add(event) {
-		this.showAddParty = false;
-		const parties = this.parties;
-		const party = event.detail;
-		parties.push(event.detail);
-		this.parties = parties;
-		this.party = undefined;
-		this.showAddParty = true;
-	}
+  add() {
+    this.show = false;
+    this.showAddParty = false;
+    this.getParties();
+    this.show = true;
+    this.showAddParty = true;
+  }
 
-	del(event) {
-		this.show = false;
-		const email = JSON.parse(JSON.stringify(event.detail.data));
-		const parties = this.parties;
-		this.showToastMsg("Success", "Party Successfully Deleted", "success");
-		this.show = true;
-	}
+  del() {
+    this.show = false;
+    this.showAddParty = false;
+    this.getParties();
+    this.show = true;
+    this.showAddParty = true;
+  }
 
-	showToastMsg(title, message, variant) {
-		this.dispatchEvent(
-			new ShowToastEvent({
-				title: title,
-				message: message,
-				variant: variant,
-			})
-		);
-	}
+  showToastMsg(title, message, variant) {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant
+      })
+    );
+  }
+
+  async getParties() {
+    const results = await getParties();
+    if (results) {
+      console.log(results);
+      this.parties = results;
+      this.renderPagination = true;
+      this.show = true;
+    } else {
+      console.log("oops");
+    }
+  }
 }
--- force-app/main/default/lwc/party/party.js
+++ force-app/main/default/lwc/party/party.js
@@ -1,161 +1,154 @@
 import { LightningElement, api, track } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import deleteParty from "@salesforce/apex/PartyService.deleteParty";
+import upsertParty from "@salesforce/apex/PartyService.upsertParty";
 
 export default class Party extends LightningElement {
-	@api party = {};
-	@api showAddParty = false;
+  @api party = {};
+  @api showAddParty = false;
 
-	sectionName;
-	modalHeading = "New Party";
-	modalContentStyle = "height:65%";
-	modalGridStyleClass = "slds-medium-size_1-of-2";
-	temp = {};
-	isNew = false;
-	show = false;
-	cols = [
-		{
-			index: 1,
-			inputs: [
-				{
-					Id: 1,
-					label: "First Name",
-					isText: true,
-					key: "firstName",
-					isRequired: true,
-				},
-				{
-					Id: 2,
-					label: "Last Name",
-					isText: true,
-					key: "lastName",
-					isRequired: true,
-				},
-				{ Id: 3, label: "Gender", isText: true, key: "gender" },
-			],
-		},
-		{
-			index: 2,
-			inputs: [
-				{ Id: 4, label: "Age", isNumeric: true, key: "age" },
-				{ Id: 5, label: "Phone", isText: true, key: "phone" },
-				{ Id: 6, label: "Address", isText: true, key: "address" },
-			],
-		},
-	];
+  get sectionName() {
+    return this.party.Party_Code__c;
+  }
+  modalHeading = "New Party";
+  modalContentStyle = "height:65%";
+  modalGridStyleClass = "slds-medium-size_1-of-2";
+  temp = {};
+  show = false;
+  cols = [
+    {
+      index: 1,
+      inputs: [
+        { Id: 1, label: "Name", isText: true, key: "Name" },
+        { Id: 2, label: "Description", isText: true, key: "Description__c" }
+      ]
+    },
+    {
+      index: 2,
+      inputs: [
+        { Id: 3, label: "Party Code", isText: true, key: "Party_Code__c" },
+        { Id: 4, label: "Chairman", isText: true, key: "Chairman__c" }
+      ]
+    }
+  ];
 
-	columns = [
-		{
-			index: 1,
-			inputs: [
-				{ Id: 1, label: "First Name", isText: true, key: "firstName" },
-				{ Id: 2, label: "Last Name", isText: true, key: "lastName" },
-				{ Id: 3, label: "Gender", isText: true, key: "gender" },
-			],
-		},
-		{
-			index: 2,
-			inputs: [
-				{ Id: 4, label: "Age", isNumeric: true, key: "age" },
-				{ Id: 5, label: "Phone", isText: true, key: "phone" },
-				{ Id: 6, label: "Address", isText: true, key: "address" },
-			],
-		},
-	];
+  columns = [
+    {
+      index: 1,
+      inputs: [
+        { Id: 1, label: "Name", isText: true, key: "Name" },
+        { Id: 2, label: "Description", isText: true, key: "Description__c" }
+      ]
+    },
+    {
+      index: 2,
+      inputs: [
+        { Id: 3, label: "Party Code", isText: true, key: "Party_Code__c" },
+        { Id: 4, label: "Chairman", isText: true, key: "Chairman__c" }
+      ]
+    }
+  ];
 
-	get isReady() {
-		return this.show;
-	}
+  get isReady() {
+    return this.show;
+  }
 
-	connectedCallback() {
-		if (this.user && !this.isNew) {
-			this.sectionName = this.user.firstName + " " + this.user.lastName;
-			this.googleMapUrl += this.user.address;
-			this.addValuesFromParent();
-		} else {
-			this.isNew = true;
-		}
-		this.show = true;
-	}
+  connectedCallback() {
+    this.addValuesFromParent();
+    this.show = true;
+  }
 
-	addValuesFromParent() {
-		this.columns.forEach((col) => {
-			col.inputs.forEach((input) => {
-				const value = this.user[input.key];
-				input.value = value;
-			});
-		});
-	}
+  addValuesFromParent() {
+    this.columns.forEach((col) => {
+      col.inputs.forEach((input) => {
+        const value = this.party[input.key];
+        input.value = value;
+      });
+    });
+  }
 
-	handleEdit() {
-		this.modalHeading = "Edit User";
-		this.template.querySelector("c-modal").openModal();
-	}
+  handleEdit() {
+    this.modalHeading = "Edit Party";
+    this.template.querySelector("c-modal").openModal();
+  }
 
-	handleOpen() {
-		this.columns = [];
-		this.columns = this.cols;
-		this.user = {};
-		this.template.querySelector("c-modal").openModal();
-	}
+  handleOpen() {
+    this.columns = [];
+    this.columns = this.cols;
+    this.user = {};
+    this.template.querySelector("c-modal").openModal();
+  }
 
-	closeConformationModal() {
-		this.template.querySelector("c-modal").closeModal();
-	}
+  closeConformationModal() {
+    this.template.querySelector("c-modal").closeModal();
+  }
 
-	handleModalConfirmation() {
-		this.show = false;
-		this.handleSaveData();
-		let message = this.isNew ? " Added" : " Modified";
-		this.showToastMsg("Success", "Party Successfully" + message, "success");
-		if (this.showAddParty) {
-			const data = this.party;
-			const dataToSend = new CustomEvent("handleadd", {
-				detail: data,
-			});
+  handleModalConfirmation() {
+    this.show = false;
+    this.handleSaveData();
+    let message = this.isNew ? " Added" : " Modified";
+    if (this.showAddParty) {
+      const data = this.party;
+      const dataToSend = new CustomEvent("handleadd", {
+        detail: data
+      });
 
-			this.dispatchEvent(dataToSend);
-		}
-		this.closeConformationModal();
-		this.show = true;
-	}
+      this.dispatchEvent(dataToSend);
+    }
+    this.showToastMsg("Success", "Party Successfully" + message, "success");
+    this.closeConformationModal();
+    this.addValuesFromParent();
+    this.show = true;
+  }
 
-	handleSaveData() {
-		this.columns.forEach((col) => {
-			col.inputs.forEach((input) => {
-				if (this.temp.hasOwnProperty(input.label)) {
-					const label = input.label;
-					input.value = this.temp[label];
-					const party = { ...this.party };
-					user[input.key] = this.temp[label];
-					this.party = party;
-					delete this.temp[label];
-				}
-			});
-		});
-	}
+  async handleSaveData() {
+    const result = await upsertParty({ party: this.party });
+    console.log(JSON.stringify(this.party));
+    console.log(result);
+  }
 
-	handleInputChanged(event) {
-		const data = event.detail;
-		this.temp[data.label] = data.value;
-	}
+  handleInputChanged(event) {
+    const keyToLabel = {
+      "Party Code": "Party_Code__c",
+      Name: "Name",
+      Description: "Description__c",
+      Chairman: "Chairman__c"
+    };
+    const data = event.detail;
+    const field = keyToLabel[data.label];
+    let party = { ...this.party };
+    party[field] = data.value;
+    this.party = undefined;
+    this.party = party;
+  }
 
-	showToastMsg(title, message, variant) {
-		this.dispatchEvent(
-			new ShowToastEvent({
-				title: title,
-				message: message,
-				variant: variant,
-			})
-		);
-	}
+  showToastMsg(title, message, variant) {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant
+      })
+    );
+  }
 
-	handleDelete() {
-		this.show = false;
-		const obj = { data: this.user.email };
-		const dataToParent = new CustomEvent("handledelete", {
-			detail: obj,
-		});
-		this.dispatchEvent(dataToParent);
-		this.show = true;
-	}
+  async handleDelete() {
+    this.show = false;
+    const result = await deleteParty({ partyId: this.party.Id });
+    if (result) {
+      this.showToastMsg(
+        "Success",
+        "Successfully Deleted the Party Record",
+        "Success"
+      );
+      console.log(result);
+      const dataToParent = new CustomEvent("handledelete", {
+        detail: this.party
+      });
+      this.show = true;
+      this.dispatchEvent(dataToParent);
+    } else {
+      this.showToastMsg("", "Failed to Delete the Party Record", "error");
+    }
+  }
 }
